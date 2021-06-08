6af49ca1f6283e57a0e59b6fc85d5cadee45f4ee
sijin-dm <sijin.li@deepmirror.com>
2021-06-08 19:50:53 +0800

    vis custom image.

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python tools/condlanenet/curvelanes/vis_curvelanes.py configs/condlanenet/curvelanes/curvelanes_small_test.py ./curvelanes_small.pth --evaluate --show --show_dst outputs
+#python tools/condlanenet/curvelanes/vis_curvelanes.py configs/condlanenet/curvelanes/curvelanes_small_test.py ./curvelanes_small.pth --evaluate --show --show_dst outputs
+python tools/condlanenet/curvelanes/vis_curvelanes.py configs/condlanenet/curvelanes/curvelanes_small_test_custom.py ./curvelanes_small.pth --show --show_dst outputs
