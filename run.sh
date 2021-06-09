d911489731e9bfc4b4a17702ee3f0a4b0f0e34ac
sijin-dm <sijin.li@deepmirror.com>
2021-06-09 15:46:03 +0800

    update .

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #python tools/condlanenet/curvelanes/vis_curvelanes.py configs/condlanenet/curvelanes/curvelanes_small_test.py ./curvelanes_small.pth --evaluate --show --show_dst outputs
-python tools/condlanenet/curvelanes/vis_curvelanes.py configs/condlanenet/curvelanes/curvelanes_small_test_custom.py ./curvelanes_small.pth --show --show_dst outputs
+#python tools/condlanenet/curvelanes/vis_curvelanes.py configs/condlanenet/curvelanes/curvelanes_small_test_custom.py ./curvelanes_small.pth --show --show_dst outputs
+python tools/pytorch2trt.py \
+    configs/condlanenet/curvelanes/curvelanes_small_test.py \
+    ./curvelanes_small.pth \
+    --out curvelanes_small.onnx \
+    --shape 1280 720
